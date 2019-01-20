00be15f0097373395d39a1b76672c57440b48eb0
Anca <anca.gheorghe1996@gmail.com>
2019-01-20 23:40:14 +0200

    add create delete edit in EquipmentController

--- ShopSnowboardEquip/Controllers/EquipmentController.cs
+++ ShopSnowboardEquip/Controllers/EquipmentController.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ShopSnowboardEquip.Data;
 using ShopSnowboardEquip.Data.interfaces;
 using ShopSnowboardEquip.Models;
 using ShopSnowboardEquip.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace ShopSnowboardEquip.Controllers
 {
@@ -13,13 +17,15 @@ namespace ShopSnowboardEquip.Controllers
     {
 		private readonly ICategoryRepository _categoryRepository;
 		private readonly IEquipmentRepository _equipmentRepository;
+        private readonly AppDbContext _context;
 
-		public EquipmentController(ICategoryRepository categoryRepository, IEquipmentRepository equipmentRepository)
+        public EquipmentController(AppDbContext context, ICategoryRepository categoryRepository, IEquipmentRepository equipmentRepository)
 		{
 			_categoryRepository = categoryRepository;
 			_equipmentRepository = equipmentRepository;
+            _context = context;
 
-		}
+        }
         public ViewResult List(string category)
         {
             string _category = category;
@@ -46,6 +52,144 @@ namespace ShopSnowboardEquip.Controllers
                 Equipment = equipments,
                 CurrentCategory = currentCategory
             });
+        }
+
+        // GET: Equipments
+        public async Task<IActionResult> Index()
+        {
+            var appDbContext = _context.Equipments.Include(e => e.Category);
+            return View(await appDbContext.ToListAsync());
+        }
+
+        // GET: Equipments/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var equipment = await _context.Equipments
+                .Include(e => e.Category)
+                .FirstOrDefaultAsync(m => m.EquipmentId == id);
+            if (equipment == null)
+            {
+                return NotFound();
+            }
+
+            return View(equipment);
+        }
+
+        // GET: Equipments/Create
+        public IActionResult Create()
+        {
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryId");
+            return View();
+        }
+
+        // POST: Equipments/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("EquipmentId,Name,ShortDescription,LongDescription,Price,ImageUrl,ImageThumbnailUrl,IsPreferredEquipment,InStock,CategoryId")] Equipment equipment)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(equipment);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryId", equipment.CategoryId);
+            return View(equipment);
+        }
+
+        // GET: Equipments/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var equipment = await _context.Equipments.FindAsync(id);
+            if (equipment == null)
+            {
+                return NotFound();
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryId", equipment.CategoryId);
+            return View(equipment);
+        }
+
+        // POST: Equipments/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int EquipmentId, [Bind("EquipmentId,Name,ShortDescription,LongDescription,Price,ImageUrl,ImageThumbnailUrl,IsPreferredEquipment,InStock,CategoryId")] Equipment equipment)
+        {
+            if (EquipmentId != equipment.EquipmentId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(equipment);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EquipmentExists(equipment.EquipmentId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryId", equipment.CategoryId);
+            return View(equipment);
+        }
+
+        // GET: Equipments/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var equipment = await _context.Equipments
+                .Include(e => e.Category)
+                .FirstOrDefaultAsync(m => m.EquipmentId == id);
+            if (equipment == null)
+            {
+                return NotFound();
+            }
+
+            return View(equipment);
+        }
+
+        // POST: Equipments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int EquipmentId)
+        {
+            var equipment = await _context.Equipments.FindAsync(EquipmentId);
+            _context.Equipments.Remove(equipment);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EquipmentExists(int id)
+        {
+            return _context.Equipments.Any(e => e.EquipmentId == id);
         }
     }
 }
